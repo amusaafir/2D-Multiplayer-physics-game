968a190013d0eff7f17b0c990620600a9b645394
Ahmed <ahmedmusaafir@gmail.com>
2016-06-25 20:18:53 +0200

    Nearly finished refactoring the server side: world object needs to be created and some other todo's mentioned in the source code.

--- server/Loader.js
+++ server/Loader.js
@@ -1,76 +1,14 @@
 // TODO: Pass Game object to Network object. The game object should require the physics.
-
-var p2 = require('p2');
+var Game = require('./Game');
 var Network = require('./Network');
 var Player = require('./Player');
 
 function Server(io) {
 	console.log('Start the game server.');
-	this.io = io;
-	this.network = new Network(io, null, p2);
-	this.load();
+	
+	this.game = new Game(io);
+	this.network = new Network(io, this.game);
 }
-
-Server.prototype.load = function() {
-	var self = this.io;
-
-	var players = [];
-	var positions = [];
-	var world;
-
-	init();
-
-	function init() {
-	    for (var x = -10; x < 10; x += 3) {
-	        for (var y = -8; y < 8; y += 3) {
-	            positions.push(new Position(x, y));
-	        }
-	    }
-
-	    world = new p2.World({ gravity: [0, 0] });
-	    world.frictionGravity = 1;
-	    world.applyDamping = true;
-	}
-
-	function Position(x, y) {
-	    this.x = x;
-	    this.y = y;
-	}
-
-	setInterval(function() {
-	    sendState();
-	}, 500);
-
-	function sendState() {
-	    var data = [];
-	    for (var i = 0; i < players.length; i++) {
-	        data.push(players[i].getClientDetails());
-	    }
-	        if (self) {
-	            self.emit('state', data);
-	        }
-	}
-
-	var aCount = 0;
-	var fixedTimeStep = 1 / 60, maxSubSteps = 10, lastTimeMilliseconds;
-
-	setInterval(function() {
-	    world.step(1 / 60);
-	}, 1000 / 60);
-
-	var currentId;
-	var currentX;
-	var currentY;
-	var request = false;
-
-	world.on("postStep", function() {
-	    if (request) {
-	        players[currentId].circleBody.applyForce([currentX, currentY], players[currentId].circleBody.position);
-	        request = false;
-	    }
-	});
-
-};
 
 module.exports = Server;
 
--- server/Network.js
+++ server/Network.js
@@ -1,4 +1,6 @@
-function Network(io, game, p2) {
+var Player = require('./Player');
+
+function Network(io, game) {
 	this.io = io;
 	this.game = game;
 	
@@ -6,42 +8,50 @@ function Network(io, game, p2) {
 }
 
 Network.prototype.connect = function() {
+	var network = this;
 	var self = this.io;
 
 	this.io.sockets.on('connect', function(socket) {
 	    self.sockets.connected[socket.id].emit('connected', true);
 
 	    socket.on('getPlayers', function() {
-	        /*var data = [];
-	        for (var i = 0; i < players.length; i++) {
-	            data.push(players[i].getClientDetails());
+	        var data = [];
+
+	        for (var i = 0; i < network.game.players.length; i++) {
+	            data.push(network.game.players[i].getClientDetails());
 	        }
-	        socket.emit('getPlayers', data);*/
+
+	        socket.emit('getPlayers', data);
 	    });
 
 	    socket.on('getWorldDetails', function() {
-	       /* var data = {
-	            time: world.time
+	        var data = {
+	            time: network.game.world.time
 	        };
-	        socket.emit('getWorldDetails', data);*/
+
+	        socket.emit('getWorldDetails', data);
 	    });
 
+	    // TODO: Add add player logic inside the game class
 	    socket.on('addMainPlayer', function() {
-	        /*var id = players.length;
-	        var player = new Player(id, positions.pop());
-	        players.push(player);
+	        var id = network.game.players.length;
+	        var player = new Player(id, network.game.positions.pop());
+	        network.game.world.addBody(player.circleBody);
+	        network.game.players.push(player);
 	        self.sockets.connected[socket.id].emit('addMainPlayer', player.getClientDetails());
-	        socket.broadcast.emit('addNewPlayer', player.getClientDetails());*/
+	        socket.broadcast.emit('addNewPlayer', player.getClientDetails());
 	    });
 
 	    socket.on('impulse', function(data) {
-	       /* currentId = data.id;
-	        currentX = data.x;
-	        currentY = data.y;
-	        request = true;
+	    	console.log('impulse received');
+	       	network.game.currentId = data.id;
+	        network.game.currentX = data.x;
+	        network.game.currentY = data.y;
+	        network.game.request = true;
+	        
 	        if (self) {
 	           	self.emit('impulseState', data);
-	        }*/
+	        }
 	    });
 	});
 };
