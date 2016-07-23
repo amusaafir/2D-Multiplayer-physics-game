4c550e9a7e025cd5087265cad28cea20d158d6f2
AJ <ahmedmusaafir@gmail.com>
2016-07-23 06:03:33 -0700

    Fixed graphics positioning issue.

--- build/bundle.js
+++ build/bundle.js
@@ -140,7 +140,6 @@ Game.prototype.postStep = function() {
 
     this.world.getWorld().on("postStep", function() {
         if (self.request) {
-            console.log('perform!');
             self.players[self.currentId].circleBody.applyForce([self.currentX, self.currentY], self.players[self.currentId].circleBody.position);
             self.request = false;
         }
@@ -229,7 +228,7 @@ Network.prototype.tryConnect = function() {
         this.connected();
     } else {
         setTimeout(function() {
-            console.log('Attempting to connect another time..');
+            console.log('Attempting to connect..');
             tryConnect();
         }, 2000);
     }
@@ -251,7 +250,6 @@ Network.prototype.getPlayers = function() {
 
     this.socket.on('getPlayers', function(playersData) {
         for (var i = 0; i < playersData.length; i++) {
-            console.log('pos' + playersData[i].position[0] + ' - ' + playersData[i].position[1]);
             self.game.addPlayer(playersData[i].id, playersData[i].position[0], playersData[i].position[1]);
             self.game.players[i].circleBody.velocity = playersData[i].velocity;
             self.game.players[i].circleBody.angularVelocity = playersData[i].angularVelocity;
@@ -276,7 +274,6 @@ Network.prototype.addMainPlayer = function() {
 
     this.socket.on('addMainPlayer', function(player) {
         self.game.mainPlayerId = player.id;
-        console.log('add mp: ' + player.position[0] + ', ' + player.position[1]);
         self.game.addPlayer(player.id, player.position[0], player.position[1]);
     });
 };
@@ -285,7 +282,6 @@ Network.prototype.addNewPlayer = function() {
     var self = this;
 
     this.socket.on('addNewPlayer', function(player) {
-        console.log('add player @ '+player.position[0]);
         self.game.addPlayer(player.id, player.position[0], player.position[1]);
     });
 };
@@ -352,50 +348,24 @@ var Player = function(id, x, y, renderer, material) {
     this.graphics = new PIXI.Graphics();
     this.graphics.lineStyle(0);
     this.graphics.beginFill(0xFFFFFF, 1);
-    console.log('inside p: ' + this.circleBody.position[0] + ', ' + this.circleBody.position[1]);
-    console.log('inside pxy: ' + x + ', ' + y);
-    this.graphics.drawCircle(this.circleBody.position[0],this.circleBody.position[1], 1);
-    this.graphics.position.x = this.circleBody.position[0];
-    this.graphics.position.y = this.circleBody.position[1];
+    this.graphics.drawCircle(0,0, 1);
+    this.graphics.position.set(this.circleBody.position[0], this.circleBody.position[1]);
     this.renderer.container.addChild(this.graphics);
 
     this.shadow = new PIXI.Graphics();
     this.shadow.lineStyle(0);
     this.shadow.beginFill(0xEEEEEE, 0.5);
-    this.shadow.drawCircle(this.circleBody.position[0],this.circleBody.position[1], 1);
-    this.shadow.position.x = this.shadowX;
-    this.shadow.position.y = this.shadowY;
+    this.shadow.drawCircle(0,0, 1);
+    this.shadow.position.set(this.circleBody.position[0], this.circleBody.position[1]);
     this.renderer.container.addChild(this.shadow);
 };
 
-Player.prototype.draw = function() { // TODO: should be called 'Transform'
-    /*this.renderer.ctx.beginPath();
-    var x = this.circleBody.position[0],
-        y = this.circleBody.position[1];
-    this.renderer.ctx.save();
-    this.renderer.ctx.translate(x, y); // Translate to the center of the circle
-    this.renderer.ctx.rotate(this.circleBody.angle); // Rotate to the circle body frame
-    this.renderer.ctx.arc(0, 0, 1, 0, 2 * Math.PI);
-    this.renderer.ctx.fillStyle = 'orange';
-    this.renderer.ctx.fill();
-    this.renderer.ctx.stroke();
-    this.renderer.ctx.restore();*/
-    var x = this.circleBody.position[0],
-        y = this.circleBody.position[1];
-    this.graphics.position.x = x;
-    this.graphics.position.y = y;
+Player.prototype.draw = function() {
+    this.graphics.position.set(this.circleBody.position[0], this.circleBody.position[1]);
 };
 
 Player.prototype.drawShadow = function() {
-    /*this.renderer.ctx.beginPath();
-    this.renderer.ctx.save();
-    this.renderer.ctx.translate(this.shadowX, this.shadowY);
-    this.renderer.ctx.rotate(this.circleBody.angle);
-    this.renderer.ctx.arc(0, 0, 1, 0, 2 * Math.PI);
-    this.renderer.ctx.stroke();
-    this.renderer.ctx.restore();*/
-     //this.shadow.position.x = this.shadowX;
-   // this.shadow.position.y =  this.shadowY;  
+    this.shadow.position.set(this.shadowX, this.shadowY);
 };
 
 module.exports = Player;
